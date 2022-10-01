90b41e0ccdf5356efb6bb5b73143897bf9809b78
Laura Ramos <marialauraramos.5@gmail.com>
2022-10-01 18:17:36 -0300

    (feat): initial backend commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 8000;
+// App
+const app = express();
+
+// API
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Expense Tracker listening on port ${PORT}`);
+})
